10fb93f7a2e1b34e85c727eabf804fe9de80372f
andymiha <andreimihaescu@hotmail.com>
2025-04-10 01:13:13 -0400

    small fixes to the nav bar

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -2,7 +2,7 @@ import { View, TouchableOpacity, Text, Image } from 'react-native';
 
 const BottomNav = () => {
   return (
-    <View className="flex-row items-center justify-around bg-[#295980] p-4">
+    <View className="flex-row items-center justify-center justify-around bg-[#295980] p-4">
       <TouchableOpacity className="items-center">
         <Image
           source={require('public/images/home.png')}
@@ -14,7 +14,7 @@ const BottomNav = () => {
       <TouchableOpacity className="items-center">
         <Image
           source={require('public/images/achievements.png')}
-          className="mb-1 h-[52px] w-[52px]"
+          className="mb-1 h-[57px] w-[57px]"
           resizeMode="contain"
         />
       </TouchableOpacity>
@@ -22,7 +22,7 @@ const BottomNav = () => {
       <TouchableOpacity className="items-center">
         <Image
           source={require('public/images/money.png')}
-          className="mb-1 h-[52px] w-[52px]"
+          className="bottom-1 mb-1 h-[54px] w-[54px]"
           resizeMode="contain"
         />
       </TouchableOpacity>
@@ -30,7 +30,7 @@ const BottomNav = () => {
       <TouchableOpacity className="items-center">
         <Image
           source={require('public/images/settings.png')}
-          className="mb-1 h-[52px] w-[52px]"
+          className="mb-1 h-[53px] w-[53px]"
           resizeMode="contain"
         />
       </TouchableOpacity>
