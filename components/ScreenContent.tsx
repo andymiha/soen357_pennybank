50f104bcdcb11a6ce8842f4359f679cd6b4cc6d0
andymiha <andreimihaescu@hotmail.com>
2025-04-09 21:05:54 -0400

    app init

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -12,6 +12,7 @@ export const ScreenContent = ({ title, path, children }: ScreenContentProps) => 
   return (
     <View className={styles.container}>
       <Text className={styles.title}>{title}</Text>
+      <Text className="text-gray-500">Open up the code for this screen:</Text>
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
